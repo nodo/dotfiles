c06cfe83f165c5b86bb2e49a9bcc224bf8223ebe
Andrea Nodari <nodo@github.com>
2021-11-04 13:45:05 +0100

    Fix tab size

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,8 @@ vim.g.maplocalleader = ' '
 -- Options
 vim.o.number = true
 vim.o.colorcolumn = '100'
+vim.o.shiftwidth = 2
+vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.termguicolors = true
 vim.o.signcolumn = 'yes'
