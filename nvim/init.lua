8136788686b2dfc00d804c3e23772fa369dd6ccf
Andrea Nodari <nodo@github.com>
2023-11-02 18:13:52 +0100

    More nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,7 +24,38 @@ require('lazy').setup({
   'github/copilot.vim',
 
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
 })
+
+-- [[ Configure Treesitter ]]
+-- See `:help nvim-treesitter`
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+  require('nvim-treesitter.configs').setup {
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = { 'go', 'lua', 'ruby', 'bash' },
+
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-space>',
+        node_incremental = '<c-space>',
+        scope_incremental = '<c-s>',
+        node_decremental = '<M-space>',
+      },
+    },
+  }
+end, 0)
 
 -- Copilot
 vim.keymap.set('i', '..', '<Plug>(copilot-next)')
