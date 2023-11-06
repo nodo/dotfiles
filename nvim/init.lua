b47e29468e0685a11831af88626407b2f661d55b
Andrea Nodari <nodo@github.com>
2023-11-06 15:34:30 +0100

    Neotree keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -227,3 +227,7 @@ vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { de
 vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
 vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+
+-- [[ NeoTree ]]
+vim.keymap.set("n", "\\", ":Neotree<CR>")
+vim.keymap.set("n", "|", ":Neotree reveal<CR>")
