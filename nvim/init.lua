52b1b0c53e4dcc77c888cc3cca7e0cd2f254605a
Andrea Nodari <nodo@github.com>
2023-11-17 10:07:47 +0100

    Add telescope treesitter mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -303,15 +303,14 @@ vim.keymap.set("n", "<BS>", ":noh<CR>")
 -- Enable telescope fzf native, if installed
 pcall(require("telescope").load_extension, "fzf")
 -- See `:help telescope.builtin`
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader>sr", require("telescope.builtin").oldfiles, { desc = "[S]earch [R]ecent files" })
 vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
 vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+vim.keymap.set("n", "<leader>st", require("telescope.builtin").treesitter, { desc = "[S]earch [T]reesitter symbols" })
 
 -- [[ NeoTree ]]
 vim.keymap.set("n", "\\", ":Neotree toggle<CR>")
@@ -321,4 +320,4 @@ vim.keymap.set("n", "|", ":Neotree reveal<CR>")
 vim.keymap.set("n", "<leader>t", require("neotest").run.run)
 
 -- [[ git ]]
-vim.keymap.set("n", "<leader>gb", ":Git blame_line<CR>", { desc = "[G]it [B]lame" })
+vim.keymap.set("n", "<leader>gb", ":Git blame_line<CR>", { desc = "[G]it [B]lame line" })
