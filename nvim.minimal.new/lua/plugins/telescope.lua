49650559ee89fcfebb0afdaf7badec1afb844d6f
Andrea Nodari <nodo@github.com>
2024-07-04 17:42:15 +0200

    Copilot

--- nvim.minimal.new/lua/plugins/telescope.lua
+++ nvim.minimal.new/lua/plugins/telescope.lua
@@ -11,13 +11,21 @@ return {
 					prompt_position = "top",
 				},
 			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
 		})
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]iles" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind [g]rep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[f]ind [b]buffers" })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "find buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[f]ind [h]elp" })
 		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[f]ind [r]esume" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[f]ind [b]buffers" })
+
+		-- shorter versions
+		vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "find files" })
+		vim.keymap.set("n", "<leader>,", builtin.buffers, { desc = "find buffers" })
 	end,
 }
