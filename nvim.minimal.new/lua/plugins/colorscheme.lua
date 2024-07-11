59132b5170aec61348559a24ac30baf97578a2b4
Andrea Nodari <nodo@github.com>
2024-07-11 15:13:27 +0000

    Tweaks

--- nvim.minimal.new/lua/plugins/colorscheme.lua
+++ nvim.minimal.new/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	"folke/tokyonight.nvim",
+	"rose-pine/neovim",
 	lazy = false,
 	priority = 1000,
 	opts = {},
 	config = function()
-		vim.cmd.colorscheme("tokyonight")
+		vim.cmd.colorscheme("rose-pine-moon")
 	end,
 }
