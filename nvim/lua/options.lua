a2ed48b249661d36cef9ad41b4fb74eef54013ba
Andrea Nodari <nodo@github.com>
2026-02-19 12:50:04 +0100

    autoread files

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -28,3 +28,9 @@ vim.o.tabstop = 4      -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
+
+-- Auto-reload files changed outside of Neovim (e.g. by Claude Code)
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+    command = "checktime",
+})
