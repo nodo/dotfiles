5596f5b65f0e2733f688a1a7eaa06b8bb8bb6e01
Andrea Nodari <nodo@github.com>
2026-02-19 13:50:51 +0100

    Add useful keybindings to use vim and agents

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -1,1 +1,17 @@
 vim.keymap.set("n", "<BS>", ":noh<CR>", {})
+
+-- copy relative path
+vim.keymap.set("n", "<leader>yr", function()
+	vim.fn.setreg("+", "@" .. vim.fn.expand("%") .. ":" .. vim.fn.line("."))
+end, { desc = "Copy file:line to clipboard" })
+vim.keymap.set("v", "<leader>yr", function()
+	vim.fn.setreg("+", "@" .. vim.fn.expand("%") .. ":" .. vim.fn.line("'<") .. "-" .. vim.fn.line("'>"))
+end, { desc = "Copy file:line range to clipboard" })
+
+-- copy absolute path
+vim.keymap.set("n", "<leader>ya", function()
+	vim.fn.setreg("+", "@" .. vim.fn.expand("%:p") .. ":" .. vim.fn.line("."))
+end, { desc = "Copy absolute file:line to clipboard" })
+vim.keymap.set("v", "<leader>ya", function()
+	vim.fn.setreg("+", "@" .. vim.fn.expand("%:p") .. ":" .. vim.fn.line("'<") .. "-" .. vim.fn.line("'>"))
+end, { desc = "Copy absolute file:line range to clipboard" })
